97726fb4732def2c0258eceadd6cba6cf08d3049
Terrance Chang <tchang46343@gmail.com>
2019-07-14 09:51:24 -0700

    Add message for no result

--- script.js
+++ script.js
@@ -3,13 +3,13 @@ const apiKey = 'AIzaSyCgg-72owDWeVhMRmUcYB58Gp2O0jymQvw';
 const videoBaseUrl = 'https://www.googleapis.com/youtube/v3/search';
 // const productBaseUrl = 'https://openapi.etsy.com/v2/listings/active.js?callback=processEtsyResponse';
 // const productApiKey ='53wmfi3n9gz2j9n9guxr4baw';
-
 function watchForm(){
     $('.mainScreen').submit(event =>{
         event.preventDefault();
         const searchTerm = $('.userSearch').val();
         getYouTubeData(searchTerm);
         getProductResults(searchTerm);
+        
     });
 }
 $(document).ready(function(){
@@ -17,6 +17,7 @@ $('input.searchResult').on('click', function(event){
     $("body, html").animate({ 
         scrollTop: $('div.resultsContainer').offset().top 
     }, 1000);
+    $('.instructionMessage').hide();
     
 })
 });
@@ -37,6 +38,7 @@ function getYouTubeData(searchTerm){
         throw new Error(response.statusText);
     })
     .then(responseJson => displayVideoResults(responseJson))
+    
     .catch(err => {
       $('#js-error-message').text(`Something went wrong: ${err.message}`);
     });   
@@ -44,18 +46,22 @@ function getYouTubeData(searchTerm){
 function displayVideoResults(responseJson){
     console.log(responseJson);
     $('#result-list').empty();
-    for (let i = 0; i < responseJson.items.length; i++){
-      $('#result-list').append(
-        `<h3>Title: ${responseJson.items[i].snippet.title}</h3>
-            <ul>
-            <a href = 'https://www.youtube.com/watch?v=${responseJson.items[i].id.videoId}'target=_blank'> 
-            <img src = ${responseJson.items[i].snippet.thumbnails.high.url}></a>
-            </ul>`
-        )};
-    $('.resultsContainer').removeClass('hidden');
+        if(responseJson.items.length>0){
+            for (let i = 0; i < responseJson.items.length; i++){
+                $('#result-list').append(
+                `<h3>Title: ${responseJson.items[i].snippet.title}</h3>
+                    <ul>
+                    <a href = 'https://www.youtube.com/watch?v=${responseJson.items[i].id.videoId}'target=_blank'> 
+                    <img src = ${responseJson.items[i].snippet.thumbnails.high.url}></a>
+                    </ul>`
+                )};
+         }
+        else{
+             $('#result-list').text(`There are no result for the search phrase you enter. Please try again`); 
+        }
+    $('#main').removeClass('hidden');
 
 }
-
 function getProductResults(searchTerm){
     $.ajax({
         url: 'https://openapi.etsy.com/v2/listings/active.js',
@@ -71,12 +77,17 @@ function getProductResults(searchTerm){
             api_key:'53wmfi3n9gz2j9n9guxr4baw',
             keywords: searchTerm,
             limit: 5,
+            includes:'Images:1',
+            sort_on: 'score',
+            geo_level: 'country',
+            result:0
         },
      
         // Work with the response
         success: function(response) {
             displayProductResults(response)
             console.log(response); // server response
+
         },
         //TODO: Implement Error Results
         error: function(error){
@@ -88,17 +99,19 @@ function getProductResults(searchTerm){
 function displayProductResults(responseJson){
 console.log(responseJson);
 $('#product-list').empty();
-    for (let i = 0; i < responseJson.results.length; i++){
-      $('#product-list').append(
-        `<h3>Title: ${responseJson.results[i].title}</h3>
-            <ul>
-            <li> Item Price: ${responseJson.results[i].price}</li>
-            
-            </ul>`
+    if(responseJson.results.length>0){
+        for (let i = 0; i < responseJson.results.length; i++){
+            $('#product-list').append(
+                `<h3>Title: ${responseJson.results[i].title}</h3>
+                    <ul class = "product-details">
+                    <li> Item Price: ${responseJson.results[i].price}</li>
+                    <li> <img src="${responseJson.results[i].Images[0].url_75x75}" alt= "${responseJson.results[i].description}"/></li>
+                    </ul>`
         )};
-    $('.resultsContainer').removeClass('hidden');
+    } 
+    else{
+        $('#product-list').text(`There are no result for the search phrase you enter. Please try again`); 
+    }       
 }
-
-
 $(watchForm);
 
